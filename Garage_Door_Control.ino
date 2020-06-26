1264d03f3260b71df08f268f78ced0cb4d1a6a4a
Clinton Guenther <clinton.guenther@gmail.com>
2020-06-25 20:50:50 -0500

    This revision is quite different than the previous, in that the module now supports configuration.  By strapping pins D6 and D5, the unit can be configured to control one of four different garage door types.  By appropriately strapping said pins, one of the following garage door types can be controlled: Meghan's side; Clinton's side; the West shop door; or the East shop door.

--- Garage_Door_Control.ino
+++ Garage_Door_Control.ino
@@ -10,12 +10,42 @@ const char* ssid = "CJG_GbE_2G4";
 const char* password = "GlockHK23";
  
 //Define the pins
-#define RELAY_PIN     D1            // Digital pin that connect to the coil of the relay on the shield
+#define RELAY_PIN       D0            // Digital pin that connects to the coil of the relay on the shield
+#define LED_PIN         D1            // Drive panel LED for health indication
+
+
+/*
+ * ID bits D4 and D5 are used to
+ * identify which garage door 
+ * the controller is associated with.
+ * 
+ *   Bits  +
+ *   D6 D5 | Garage Door
+ * +-------------------------------+
+ *    0  0 | Clinton's Side
+ *    0  1 | Meghan's Side
+ *    1  0 | Shop Door West
+ *    1  1 | Shop Door East
+ *         +
+ *
+ */
+
+#define CJG_SIDE        0             // Bits D3 and D2 = 0b00
+#define MEG_SIDE        1             // Bits D3 and D2 = 0b01
+#define SHOP_DOOR_WEST  2             // Bits D3 and D2 = 0b10
+#define SHOP_DOOR_EAST  3             // Bits D3 and D2 = 0b11
+
 #define PUSH_DWELL_US 900           // Dwell time, in us, for 'holding' down the button
  
 //Define MQTT parameters 
-#define mqtt_server "192.168.0.249"           // This is the host address of HASSIO (reserved address in router)
-#define button_topic "sensor/garage/action1"  // You can change this name, but make sure you "replace all"
+#define mqtt_server "192.168.0.249"                   // This is the host address of HASSIO (reserved address in router)
+
+#define BUTTON_TOPIC_CJG        "sensor/garage/cjg_action"   
+#define BUTTON_TOPIC_MEG        "sensor/garage/meg_action"           
+#define BUTTON_TOPIC_SHOP_WEST  "sensor/garage/shop_west_action"           
+#define BUTTON_TOPIC_SHOP_EAST  "sensor/garage/shop_east_action"           
+
+
 const char* mqtt_user = "homeassistant";      // As defined in configuration.YAML
 const char* mqtt_pass = "GlockHK23";          // As defined in configuration.YAML
  
@@ -29,16 +59,44 @@ WiFiClient espCgarage1;
 PubSubClient client(espCgarage1);
  
 //Setup Variables
-String switch1;
-String strTopic;
+String          switch1;
+String          strTopic;
+int             configured_side = 99;
  
 void setup() {
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
+  
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  
+  /* Define ID pins as inputs */
+  pinMode(D5, INPUT);
+  pinMode(D6, INPUT);
  
   Serial.begin(115200);
   Serial.println("Reset");
 
+  configured_side = (int)(digitalRead(D6) << 1 | digitalRead(D5));
+  
+  switch (configured_side) {
+    case CJG_SIDE:
+      Serial.println("ID configired as Clinton's side.");
+    break;
+    case MEG_SIDE:
+      Serial.println("ID configired as Meghan's side.");
+    break;
+    case SHOP_DOOR_WEST:
+      Serial.println("ID configired as shop door West.");
+    break;
+    case SHOP_DOOR_EAST:
+      Serial.println("ID configired as shop door East.");
+    break;
+    default:
+      Serial.println("ID improperly configured.");
+    break;
+  }
+  
   setup_wifi();
 
   client.setServer(mqtt_server, 1883);  //1883 is the port number you have forwared for mqtt messages.
@@ -48,31 +106,84 @@ void setup() {
 
 void loop() {
   //If MQTT client can't connect to broker, then reconnect
-  if (!client.connected()) {
+  while (!client.connected()) {
+    digitalWrite(LED_PIN, LOW);            
     Serial.println("Reconnecting...");
     setup_wifi();                         // The router could have kicked us off...
     reconnect();
+    delay(500);
   }
-  client.loop(); //the mqtt function that processes MQTT messages
+  
+  digitalWrite(LED_PIN, HIGH);    
+  client.loop();                  // the mqtt function that processes MQTT messages
   
 }
  
 void callback(char* topic, byte* payload, unsigned int length) {
   payload[length] = '\0';
   strTopic = String((char*)topic);
-  if (strTopic == button_topic)
+  if (strTopic == BUTTON_TOPIC_CJG && configured_side == CJG_SIDE)
   {
     switch1 = String((char*)payload);
-    Serial.println(switch1);
+    Serial.print("Topic: "); Serial.println(strTopic);
+    Serial.print("Payload: ");Serial.println(switch1);
     if (switch1 == "PUSH_BUTTON")
     {
       // 'Push' the garage door open/close button
-      Serial.println("ON");
+      Serial.println("RELAY ACTIVE");
       digitalWrite(RELAY_PIN, HIGH);
       delay(PUSH_DWELL_US);
       digitalWrite(RELAY_PIN, LOW);
+      Serial.println("RELAY INACTIVE");
     }
   }
+
+  else if (strTopic == BUTTON_TOPIC_MEG && configured_side == MEG_SIDE)
+  {
+    switch1 = String((char*)payload);
+    Serial.print("Topic: "); Serial.println(strTopic);
+    Serial.print("Payload: ");Serial.println(switch1);
+    if (switch1 == "PUSH_BUTTON")
+    {
+      // 'Push' the garage door open/close button
+      Serial.println("RELAY ACTIVE");
+      digitalWrite(RELAY_PIN, HIGH);
+      delay(PUSH_DWELL_US);
+      digitalWrite(RELAY_PIN, LOW);
+      Serial.println("RELAY INACTIVE");
+    }
+  }
+  
+  else if (strTopic == BUTTON_TOPIC_SHOP_WEST && configured_side == SHOP_DOOR_WEST)
+  {
+    switch1 = String((char*)payload);
+    Serial.print("Topic: ");    Serial.println(strTopic);
+    Serial.print("Payload: ");  Serial.println(switch1);
+    if (switch1 == "PUSH_BUTTON") {
+      // 'Push' the garage door open/close button
+      Serial.println("RELAY ACTIVE");
+      digitalWrite(RELAY_PIN, HIGH);
+      delay(PUSH_DWELL_US);
+      digitalWrite(RELAY_PIN, LOW);
+      Serial.println("RELAY INACTIVE");
+    }
+  }
+  
+  else if (strTopic == BUTTON_TOPIC_SHOP_EAST && configured_side == SHOP_DOOR_EAST)
+  {
+    switch1 = String((char*)payload);
+    Serial.print("Topic: ");    Serial.println(strTopic);
+    Serial.print("Payload: ");  Serial.println(switch1);
+    if (switch1 == "PUSH_BUTTON") {
+      // 'Push' the garage door open/close button
+      Serial.println("RELAY ACTIVE");
+      digitalWrite(RELAY_PIN, HIGH);
+      delay(PUSH_DWELL_US);
+      digitalWrite(RELAY_PIN, LOW);
+      Serial.println("RELAY INACTIVE");
+    }
+  }
+  
 }
 
 void setup_wifi() {
--- Readme.md
+++ Readme.md
@@ -4,13 +4,25 @@ This repository contains the source file for the WEMOS D1 Mini (lite) whose job 
 
 It is the goal of this project to keep the following information updated.  For troubleshooting purposes, the MAC address of the WEMOS D1 Mini used within the garage door opener mechanism is DC-4F-22-CF-B5-40.  To make identifying the apparatus in the WiFi Client table effortless, its host name was renamed to *WEMOS_D1_Mini_Garage_Door*
 
+Starting with version __2.0.0__, the unit can be configured to control one of four different garage door types.  The following table describes how configuration is accomplished by properly strapping ID pins D5 and D6.    
+
+Bits (D6:D5)    | Garage Door
+:-------------: | :--------------: 
+00              | Clinton's Side
+01              | Meghan's Side 
+10              | Shop Door West
+11              | Shop Door East
+
+
 ### TOOL SUITE ###
 
 The source file(s) are to be compiled using Arduino IDE v1.8.8, or newer.    
 
 It is necessary to include esp8266 libraries by including the URL to the repo.  Navigate to File > Preferences, and add the following URL: http://arduino.esp8266.com/stable/package_esp8266com_index.json
 
-After pointing to esp8266 libraries, it is necessary to go to tools > board > boards manager, and install the ESP8266 manager.  The one we want will be written by the "community".    Prior to compiling, it's necessary to choose board "LOLIN(WEMOD) D1 Mini Lite".  
+After pointing to esp8266 libraries, it is necessary to go to tools > board > boards manager, and install the ESP8266 manager.  The one we want will be written by the "community".    Prior to compiling, it's necessary to choose board "LOLIN(WEMOS) D1 Mini Lite".  
 
 ### VERSIONS ###
 * V1.0.0 -- There were a few releases prior to this version.  Just prior to this release, there was an issue in which the garage door button on the HA dashboard would randomly stop working.  When investigating, it was clear the module was attempting to reconnect to the MQTT server, however, the issue was that it was not first attempting to reconnect to the WiFi router -- which it was getting kicked off of.  This version includes provisions to verify a WiFi connection is stable prior to attempting to reconnect to the MQTT server.  Also, the button-push-dwell was increased to 900ms.  
+
+* v2.0.0 -- This revision is quite different than the previous, in that the module now supports configuration.  By strapping pins D6 and D5, the unit can be configured to control one of four different garage door types.  By appropriately strapping said pins, one of the following garage door types can be controlled: Meghan's side; Clinton's side; the West shop door; or the East shop door.  
